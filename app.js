ec98d8d72afc7c9cc301a870e450d850410e242b
Alonsso <cardenasjairods@gmail.com>
2020-10-11 23:46:38 -0500

    Final

---
+++ app.js
@@ -0,0 +1,28 @@
+/* GET : http://localhost:9000/aliens
+    GET : http://localhost:9000/aliens/<id>
+    POST : http://localhost:9000/aliens
+    PATCH : http://localhost:9000/aliens/<id>
+*/
+
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
+
+const app = express()
+
+mongoose.connect(url, {useNewUrlParser:true})
+
+const con = mongoose.connection
+
+con.on('open', function(){
+    console.log('connected...')
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens', alienRouter)
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => `Server running on port ${port} 🔥`);
